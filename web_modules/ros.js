937851a6e700e19723fed368c825595ef5a36539
Jesse Clark <jjclark1982@gmail.com>
2016-07-12 15:13:23 +1000

    reconnect to rosbridge after close

--- web_modules/ros.js
+++ web_modules/ros.js
@@ -1,21 +1,35 @@
 global.EventEmitter2 = require('eventemitter2');
 var ROSLIB = require('roslib');
 
-// define a singleton with the live websocket connection
-var ros = new ROSLIB.Ros({
-  url : 'ws://192.168.99.100:9090'
-});
-ros.on('error', function(error) {
-  console.log(error);
-});
+var ros = null;
+var connectInterval = null;
 
-// Find out exactly when we made a connection.
-ros.on('connection', function() {
-  console.log('ROSBridge Connection made!');
-});
+function connect() {
+    if (connectInterval) {
+        clearInterval(connectInterval);
+        connectInterval = null;
+    }
 
-ros.on('close', function() {
-  console.log('ROSBridge Connection closed.');
-});
+    // define a singleton with the live websocket connection
+    ros = new ROSLIB.Ros({
+        url : 'ws://192.168.99.100:9090'
+    });
+    ros.on('error', function(error) {
+        console.log('ROSBridge error', error);
+    });
+
+    // Find out exactly when we made a connection.
+    ros.on('connection', function() {
+        console.log('ROSBridge Connection made!');
+    });
+
+    ros.on('close', function() {
+        console.log('ROSBridge Connection closed. Reconnecting.');
+        connectInterval = setInterval(function(){
+            connect();
+        }, 1000);
+    });
+}
+connect();
 
 export {ros, ROSLIB}
