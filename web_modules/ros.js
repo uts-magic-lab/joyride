d425c56b624f7af2a487fea00ed09ee806321bae
Jesse Clark <jjclark1982@gmail.com>
2016-07-13 11:45:51 +1000

    reconnect to rosbridge without reinstantiating `ros` object. should prevent the need for page reload.

--- web_modules/ros.js
+++ web_modules/ros.js
@@ -1,35 +1,25 @@
 global.EventEmitter2 = require('eventemitter2');
 var ROSLIB = require('roslib');
 
-var ros = null;
-var connectInterval = null;
+// define a singleton with the live websocket connection
+var ros = new ROSLIB.Ros({
+    url: process.env.ROSBRIDGE_URI
+});
 
-function connect() {
-    if (connectInterval) {
-        clearInterval(connectInterval);
-        connectInterval = null;
-    }
+ros.on('error', function(error) {
+    console.log('ROSBridge error', error);
+});
 
-    // define a singleton with the live websocket connection
-    ros = new ROSLIB.Ros({
-        url: process.env.ROSBRIDGE_URI
-    });
-    ros.on('error', function(error) {
-        console.log('ROSBridge error', error);
-    });
+// Find out exactly when we made a connection.
+ros.on('connection', function() {
+    console.log('ROSBridge Connection made!');
+});
 
-    // Find out exactly when we made a connection.
-    ros.on('connection', function() {
-        console.log('ROSBridge Connection made!');
-    });
-
-    ros.on('close', function() {
-        console.log('ROSBridge Connection closed. Reconnecting.');
-        connectInterval = setInterval(function(){
-            connect();
-        }, 1000);
-    });
-}
-connect();
+ros.on('close', function() {
+    console.log('ROSBridge Connection closed. Reconnecting.');
+    setTimeout(function(){
+        ros.connect(process.env.ROSBRIDGE_URI);
+    }, 5000);
+});
 
 export {ros, ROSLIB}
