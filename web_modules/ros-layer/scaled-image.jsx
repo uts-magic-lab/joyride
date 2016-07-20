f0bdcf49285881bffdea4800203f59b48ae02781
Jesse Clark <jjclark1982@gmail.com>
2016-07-20 21:16:15 +1000

    remove unused code from scaled-image

--- web_modules/ros-layer/scaled-image.jsx
+++ web_modules/ros-layer/scaled-image.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
 
 export default class ScaledImage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.sendMessage = this.sendMessage.bind(this);
-    }
-
-    componentDidMount() {
-        this.topic = ros.Topic({
-            name: this.props.topic,
-            messageType: 'std_msgs/String'
-        });
-    }
-
-    sendMessage(event) {
-        event.preventDefault();
-        this.topic.publish({
-            data: event.target.messageData.value
-        })
-    }
-
     render() {
+        const url = this.props.src.replace(/ /g, '%20');
         return (
             <div
                 className="scaled-image"
-                style={{backgroundImage: 'url('+this.props.src.replace(/ /g, '%20')+')'}}
+                style={{backgroundImage: 'url('+url+')'}}
             />
         )
     }
