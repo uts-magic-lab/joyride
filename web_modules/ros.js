4fd4d2da26a91a0f39d981df8f22d62e0376da6c
Jesse Clark <jjclark1982@gmail.com>
2016-08-01 22:03:18 +1000

    differentiate between initial connection and reconnection

--- web_modules/ros.js
+++ web_modules/ros.js
@@ -4,36 +4,41 @@ import rosout from './rosout';
 import statusDisplay from 'status-display';
 
 // define a singleton with the live websocket connection
-statusDisplay.set('Connecting', 'Connecting to ROSBridge');
+statusDisplay.set('Connecting', 'Connecting to ROS');
 var ros = new ROSLIB.Ros({
     url: process.env.ROSBRIDGE_URI
 });
-
+ros._everConnected = false;
 // make logging functions available from the shared instance
 Object.assign(ros, rosout(ros));
 
 ros.on('error', function(error) {
-    statusDisplay.set('Error', error);
+    statusDisplay.set('Error', 'Error connecting to ROS');
 });
 
-// Find out exactly when we made a connection.
 ros.on('connection', function connected() {
+    ros._everConnected = true;
     console.log(`Connected to ROSBridge ${ros.socket.url}`);
     if (statusDisplay.get() == 'Connecting') {    
-        statusDisplay.set('OK', `Connected to ROSBridge`);
+        statusDisplay.set('OK', `Connected to ROS`);
     }
     ros.loginfo(`${process.env.PACKAGE_NAME} v${process.env.PACKAGE_VERSION}` +
         ` started at ${document.location} on ${navigator.userAgent}`);
 });
 
 ros.on('close', function() {
-    statusDisplay.set('Error', 'ROSBridge Connection closed. Reconnecting.');
+    if (!ros._everConnected) {
+        statusDisplay.set('Connecting', 'Connecting to ROS');
+    }
+    else {
+        statusDisplay.set('Error', 'ROS Connection closed. Reconnecting.');
+    }
     setTimeout(function(){
         ros.connect(process.env.ROSBRIDGE_URI);
+        // note: other modules still need to re-subscribe to everything.
+        // reloading the page will work,
+        // but it may be possible to re-mount components
     }, 5000);
-    // note: other modules still need to re-subscribe to everything.
-    // reloading the page is overkill
-    // but it may be possible to re-mount components
 });
 
 export {ros, ROSLIB}
